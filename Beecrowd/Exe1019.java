4801fd4a75eaa95ae0464b05efb44af3b24a0b74
brunospbnu <brunosp423@gmail>
2026-03-18 10:01:03 -0300

    beecrowd

---
+++ Beecrowd/Exe1019.java
@@ -0,0 +1,28 @@
+package Beecrowd;
+/*
+Entradas: 
+n (tempo em segundos de um evento)
+Saida:
+horas, minutos, segundos.
+Algoritmo:
+ler n
+horas = n / 3600
+minutos = (n % 3600) / 60
+segundos = (n % 3600) %60
+escrever horas+":"+minutos+":"+segundos
+*/
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Exe1019 {
+    public static void main(String[] args) {
+    Scanner teclado = new Scanner (System.in);
+    int n = teclado.nextInt();
+    int horas = n / 3600;
+    int minutos = (n % 3600) / 60;
+    int segundos = (n % 3600) % 60;
+    System.out.println(horas+":"+minutos+":"+segundos);
+    teclado.close();
+    }
+}
