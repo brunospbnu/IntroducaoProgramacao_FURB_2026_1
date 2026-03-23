9626fc3e51462ad61fe41e516c8ca152bfb43f17
brunospbnu <brunosp423@gmail>
2026-03-23 10:31:37 -0300

    Add new exercise Uni4Exe07 to calculate postage cost based on letter weight

---
+++ Unidade4/Uni4Exe07.java
@@ -0,0 +1,46 @@
+package Unidade4;
+
+import java.util.Scanner;
+
+/*
+Análise:
+O custo do selo de uma carta com até 50 gramas é de R$ 0,45. 
+As cartas com peso superior pagam um adicional de R$ 0,45 por cada 20 gramas, ou fração, em que excedem aquele peso.
+Escreva um algoritmo que dado o peso da carta, em gramas, determine o custo do selo.
+OK.
+
+Entradas:
+pesoCarta
+
+
+Saida:
+custoSelo
+
+
+Algoritmo:
+ler pesoCarta
+Se pesoCarta <= 50, então
+custoSelo = 0.45
+senão
+custoSelo = 0.45 + (0.45 *((pesoCarta - 50)/20 + 1)
+escrever custoSelo
+FIM
+
+*/
+public class Uni4Exe07 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o Peso da Carta");
+        int pesoCarta = teclado.nextInt();
+        double custoSelo = 0.45;
+        double custoExtra = 0;
+        if (pesoCarta <= 50) {
+            System.out.println("O custo do Selo é de R$ " + custoSelo);
+        }
+        else {
+            custoExtra = 0.45 *(((pesoCarta - 50)/20) + 1);
+            custoSelo = custoSelo + custoExtra;
+            System.out.println("O custo do Selo é de R$ " + custoSelo);}
+        teclado.close();
+    }
+    }
