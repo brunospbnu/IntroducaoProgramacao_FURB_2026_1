ef6fe9f2ccad58e4cb122eb30c928a39260f68a1
brunospbnu <brunosp423@gmail.com>
2026-04-03 13:14:02 -0300

    Adiciona lógica para carregar e salvar saldo em arquivo no exercício Uni4Exe30Desafio

--- Unidade4/Uni4Exe30Desafio.java
+++ Unidade4/Uni4Exe30Desafio.java
@@ -1,6 +1,9 @@
 package Unidade4;
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class Uni4Exe30Desafio {
     public static void main(String[] args) throws InterruptedException {
@@ -9,6 +12,27 @@ public class Uni4Exe30Desafio {
         int opcao = 0;
         double saldo = 0;
         double numeroDaOperacao = 0;
+
+        try {
+            // 1. Aponta para o arquivo físico no seu computador
+            File arquivo = new File("saldo.txt");
+            
+            // 2. Cria o Scanner, mas passa o ARQUIVO no lugar do System.in!
+            Scanner leitorDeArquivo = new Scanner(arquivo);
+            leitorDeArquivo.useLocale(java.util.Locale.US);
+            saldo = leitorDeArquivo.nextDouble();
+
+            // 4. Fecha o leitor (sempre importante!)
+            leitorDeArquivo.close();
+            
+            System.out.println("Saldo carregado com sucesso!");
+            
+        } catch (Exception erro) {
+            // Se for a 1ª vez que o programa roda, o arquivo não existe ainda. Ele cai aqui.
+            System.out.println("Nenhum Saldo salvo encontrado. Iniciando do zero...");
+        }
+
+
         do {
             System.out.println("MENU: \n 1 - Ver saldo \r\n" + //
                                 "\r\n" + //
@@ -31,8 +55,11 @@ public class Uni4Exe30Desafio {
             else if (opcao == 3) {
                 System.out.print("Informe o valor a ser sacado: ");
                 numeroDaOperacao = teclado.nextDouble();
-                saldo = saldo - numeroDaOperacao;
-                System.out.println("Saque concluído");
+                if (numeroDaOperacao <= saldo) {saldo = saldo - numeroDaOperacao;
+                    System.out.println("Saque concluído");
+                } else {
+                    System.out.println("Operação negada: Saldo insuficiente!");
+                }
             }
             else if (opcao == 4) {
                 System.out.println("Encerando o Caixa Eletrônico");
@@ -44,5 +71,17 @@ public class Uni4Exe30Desafio {
 
         } while (opcao != 4);
 
-        teclado.close();}
-    }
+        try {
+            FileWriter arquivo = new FileWriter("Saldo.txt");
+            PrintWriter gravador = new PrintWriter(arquivo);
+            gravador.println(saldo);
+            gravador.close();
+        }
+        catch (Exception erro) {
+            // Se o HD estiver cheio ou a pasta bloqueada, ele não "quebra" o programa, cai aqui:
+            System.out.println("Ops! Não foi possível salvar o arquivo: " + erro.getMessage());
+        }
+
+        teclado.close();
+    
+    }}
