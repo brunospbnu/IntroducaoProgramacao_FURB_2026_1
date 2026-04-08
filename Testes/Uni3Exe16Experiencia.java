88ccccf00dc9ea0ff4d1a4938e5642ae4d7a6265
brunospbnu <brunosp423@gmail>
2026-04-08 10:29:25 -0300

    Atualiza a calculadora de troco para incluir notas de 50, 20, 5 e moedas de 1, 0.50, 0.25, 0.10, 0.05 e 0.01, além de melhorar a mensagem de resultado.

--- Testes/Uni3Exe16Experiencia.java
+++ Testes/Uni3Exe16Experiencia.java
@@ -29,7 +29,7 @@ public class Uni3Exe16Experiencia {
     FIM
     */
     public static void main(String[] args){
-        JFrame janela = new JFrame("Calculadora de Troco");
+        JFrame janela = new JFrame("Calculadora de Troco - Usar ponto para separar centavos");
         janela.setSize(400, 200);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setLayout(new FlowLayout());
@@ -50,16 +50,49 @@ public class Uni3Exe16Experiencia {
             double valorTotalCompra = Double.parseDouble(textValorTotalCompra.getText());
             double valorDadoPeloCliente = Double.parseDouble(textValorDadoPeloCliente.getText());
             double diferenca = valorDadoPeloCliente - valorTotalCompra;
-            int Notas100 = (int) (diferenca / 100);
-            int Notas10 = (int) (diferenca % 100) / 10;
-            int Notas1 = (int) ((diferenca % 100) % 10);
-            int numMinNotas = Notas100 + Notas10 + Notas1;
-        labelResultado.setText("<html>O número mínimo de notas de troco é: " + numMinNotas +
-                "<br>Quantidade de Notas de 100 necessárias: " + Notas100 +
-                "<br>Quantidade de Notas de 10 necessárias: " + Notas10 +
-                "<br>Quantidade de Notas de 1 necessárias: " + Notas1 + "</html>");
-    
+            int Notas100 = (int) diferenca / 100;
+            diferenca = (diferenca % 100);
+            int Notas50 = (int) diferenca / 50;
+            diferenca = diferenca  % 50;
+            int Notas20 = (int) diferenca / 20;
+            diferenca = diferenca % 20;
+            int Notas10 =(int) diferenca / 10;
+            diferenca = diferenca % 10;
+            int Notas5 = (int) diferenca / 5;
+            diferenca = diferenca % 5;
+            int Notas2 = (int) diferenca / 2;
+            diferenca = diferenca % 2;
+            int Moedas1 = (int) diferenca / 1;
+            diferenca = (diferenca % 1)*100;
+            int Moedas050 = (int) diferenca / 50;
+            diferenca = diferenca % 50;
+            int Moedas025 = (int) diferenca / 25;
+            diferenca = diferenca % 25;
+            int Moedas010 = (int) diferenca /10;
+            diferenca = diferenca % 10;
+            int Moedas005 = (int) diferenca /5;
+            diferenca = diferenca % 5;
+            int Moedas001 = (int) diferenca /1;
 
+
+
+            int numMinNotas = Notas100 + Notas50 + Notas20 + Notas10 + Notas5 + Notas2;
+        labelResultado.setText("<html>O número mínimo de notas de troco é: " + numMinNotas +
+                "<br> Quantidade de Notas / Moedas necessárias: <br> " 
+                + "<br>" + Notas100 + " nota(s) de R$ 100.00; " 
+                + "<br>" + Notas50 + " nota(s) de R$ 50.00; " 
+                + "<br>" + Notas20 + " nota(s) de R$ 20.00; "
+                + "<br>" + Notas10 + " nota(s) de R$ 10.00; "
+                + "<br>" + Notas5 + " nota(s) de R$ 5.00; "
+                + "<br>" + Notas2 + " nota(s) de R$ 2.00; "
+                + "<br>" + Moedas1 + " moeda(s) de R$ 1.00; "
+                + "<br>" + Moedas050 + " moeda(s) de R$ 0.50; "
+                + "<br>" + Moedas025 + " moeda(s) de R$ 0.25; "
+                + "<br>" + Moedas010 + " moeda(s) de R$ 0.10; "
+                + "<br>" + Moedas005 + " moeda(s) de R$ 0.05; "
+                + "<br>" + Moedas001 + " moeda(s) de R$ 0.01; "
+                 + "</html>");
+    
     }}
         
     );
