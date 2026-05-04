f3dd33e938b28115fd5cad1ac5cd5c3474311f02
brunospbnu <brunosp423@gmail.com>
2026-05-04 19:09:54 -0300

    Refatora cálculo de quilometragem e corrige variáveis para controle de abastecimentos

--- Unidade5/Uni5Exe13.java
+++ Unidade5/Uni5Exe13.java
@@ -8,31 +8,35 @@ public class Uni5Exe13 {
         System.out.println("Programa de Controle de abastecimentos e KM / Litro");
         System.out.print("Informe o total de reabastecimentos: ");
         int reabastecimentos = teclado.nextInt();
-        double quilometragemPercorrida = 0;
+        double quilometragemAtual = 0;
+        double quilomatragemTotal = 0;
         double litrosAtuais = 0; 
+        double litrosTotais = 0;
         double km_litroAtual = 0;
-        double km_litroTotal = 0;
 
         for (int contagem = 1; contagem <= reabastecimentos; contagem++) {
 
             System.out.println("Parada " + contagem + " Informe os dados: ");
             System.out.print("Quilometragem: ");
-            quilometragemPercorrida = teclado.nextDouble();
+            quilometragemAtual = teclado.nextDouble();
             System.out.print("Combustível: "); 
             litrosAtuais = teclado.nextDouble();
             
-            km_litroAtual = quilometragemPercorrida / litrosAtuais;
+            km_litroAtual = quilometragemAtual / litrosAtuais;
 
-            km_litroTotal = km_litroTotal + km_litroAtual;
+            quilomatragemTotal = quilomatragemTotal + quilometragemAtual;
+
+            litrosTotais = litrosTotais + litrosAtuais;
 
             System.out.println("Resultado Parada " + contagem + ": " + km_litroAtual + " km por litro");
 
         }
 
-        double media = km_litroTotal / reabastecimentos;
+        double media = quilomatragemTotal / litrosTotais;
         System.out.println("Quilometragem média obtida por litro: " + media);
 
         teclado.close();
     }
     
 }
+
