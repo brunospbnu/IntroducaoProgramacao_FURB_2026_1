3775fa3cd1b3f88680a4486f9dcc96c0dd1d52e6
brunospbnu <brunosp423@gmail.com>
2026-05-15 20:40:00 -0300

    Refatora Uni5Exe25 para simplificar a lógica de controle de pontos e melhorar a validação de entrada

--- Unidade5/Uni5Exe25.java
+++ Unidade5/Uni5Exe25.java
@@ -11,78 +11,48 @@ public class Uni5Exe25 {
                             "\r\n Precione ENTER para continuar");
        teclado.nextLine();
 
-       System.out.print("Iniciando: \n\r Digite E (ponto da esquerda) ou D (ponto da direita): ");
-
-       char pontoAtual = teclado.next().charAt(0);
-       pontoAtual = Character.toLowerCase(pontoAtual);
        int somaDosPontosE = 0;
        int somaDosPontosD = 0;
 
-        while (pontoAtual != 'e' && pontoAtual !='d') {
-            System.out.print("Informação Incorreta !!! \n\r Digite E (ponto da esquerda) ou D (ponto da direita): ");
-            pontoAtual = teclado.next().charAt(0);
-            pontoAtual = Character.toLowerCase(pontoAtual);
-        }
 
-        while ((somaDosPontosD < 21 && somaDosPontosE < 21)) {
+        while ((somaDosPontosD < 21 && somaDosPontosE < 21 || Math.abs(somaDosPontosE - somaDosPontosD) < 2)) {
+            System.out.print("\nPlacar: E [" + somaDosPontosE + "] x [" + somaDosPontosD + "] D");
+            System.out.print("\nDigite E ou D para marcar o ponto: ");
+
+            char pontoAtual = Character.toLowerCase(teclado.next().charAt(0));
+
+            // Validação de erro
+            while (pontoAtual != 'e' && pontoAtual != 'd') {
+                System.out.print("Informação Incorreta!!! \nDigite E ou D: ");
+                pontoAtual = Character.toLowerCase(teclado.next().charAt(0));
+            }
+
+            // Atribui o ponto
             if (pontoAtual == 'e') {
                 somaDosPontosE++;
-            }
-            else if (pontoAtual =='d') {
+            } else {
                 somaDosPontosD++;
             }
 
-            if ((somaDosPontosD < 21 && somaDosPontosE < 21)) {
+            // Aviso de "Vai a dois"
+            if (somaDosPontosE >= 20 && somaDosPontosD >= 20 && somaDosPontosE == somaDosPontosD) {
+                System.out.println("=> Empate! O jogo vai a dois!");
+            }
                 
-            
-            System.out.print("Proxima Rodada: \n\r Digite E (ponto da esquerda) ou D (ponto da direita): ");
-
-            pontoAtual = teclado.next().charAt(0);
-            pontoAtual = Character.toLowerCase(pontoAtual);
-            
-                while (pontoAtual != 'e' && pontoAtual !='d') {
-                System.out.print("Informação Incorreta !!! \n\r Digite E (ponto da esquerda) ou D (ponto da direita): ");
-                pontoAtual = teclado.next().charAt(0);
-                pontoAtual = Character.toLowerCase(pontoAtual);
-                }
-            }
        }
+    
+// TELA DE FINALIZAÇÃO (Quando o while der Falso, ele cai direto aqui!)
+        System.out.println("\n==================================");
+        System.out.println("          FIM DE JOGO !!!         ");
+        System.out.println("==================================");
 
-       if ((somaDosPontosD - somaDosPontosE >= 2) || (somaDosPontosE - somaDosPontosD >= 2)) {
-        
-       
-            while ((somaDosPontosD > 21 && somaDosPontosE > 21)) {
-                System.out.println("'Vai a Dois':");
-            
-                if (pontoAtual == 'e') {
-                somaDosPontosE++;
-                }
-                else if (pontoAtual =='d') {
-                somaDosPontosD++;
-            }
-
-            System.out.print("Proxima Rodada: \n\r Digite E (ponto da esquerda) ou D (ponto da direita): ");
-
-            pontoAtual = teclado.next().charAt(0);
-            pontoAtual = Character.toLowerCase(pontoAtual);
-            
-                while (pontoAtual != 'e' && pontoAtual !='d') {
-                    System.out.print("Informação Incorreta !!! \n\r Digite E (ponto da esquerda) ou D (ponto da direita): ");
-                    pontoAtual = teclado.next().charAt(0);
-                    pontoAtual = Character.toLowerCase(pontoAtual);
-                    }
-       }
-    }
-
-       if (somaDosPontosE > somaDosPontosD) {
-            System.out.println("FIM DE JOGO !!! \n\r O vencedor é o jogador da ESQUERDA !!!");
-            System.out.println("Pontos da Esquerda = " + somaDosPontosE + " / Pontos da Direita = " + somaDosPontosD);
-       }
-
-       else if (somaDosPontosD > somaDosPontosE) {
-            System.out.println("FIM DE JOGO !!! \n\r O vencedor é o jogador da DIREITA !!! ");
-            System.out.println("Pontos da Direita = " + somaDosPontosD + " / Pontos da Esquerda = " + somaDosPontosE);
+        if (somaDosPontosE > somaDosPontosD) {
+            System.out.println("O vencedor é o jogador da ESQUERDA !!!");
+        } else {
+            System.out.println("O vencedor é o jogador da DIREITA !!! ");
         }
+
+        System.out.println("Pontos da Esquerda = " + somaDosPontosE + " / Pontos da Direita = " + somaDosPontosD);
 
         teclado.close();
     }
