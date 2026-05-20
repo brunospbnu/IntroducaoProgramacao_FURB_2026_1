13a5b5b091f9a0825717defa420a96cd95fc245b
brunospbnu <brunosp423@gmail>
2026-05-20 08:53:45 -0300

    Refatora classe Uni6Exe07 para corrigir a validação de entrada e implementar a ordenação do vetor

--- Unidade6/Uni6Exe07.java
+++ Unidade6/Uni6Exe07.java
@@ -14,7 +14,7 @@ public class Uni6Exe07 {
 
         int vetorOrdenado[] = new int[n];
 
-        vetorOrdenado = ordenarVetor(vetorInteiro, vetorOrdenado);
+        vetorOrdenado = ordenarVetor(vetorInteiro);
 
         informarVetorResultante(vetorInteiro, vetorOrdenado);
     }
@@ -23,7 +23,7 @@ public class Uni6Exe07 {
         System.out.print("Digite o Valor de N (limitado a 20 posições): ");
         int n = s.nextInt();
         while ( n > 20 ) {
-            System.out.print("ERRO: Valor informado maior que 20, digite um valor válido");
+            System.out.print("ERRO: Valor informado maior que 20, digite um valor válido: ");
             n = s.nextInt();
         }
         return n;
@@ -31,23 +31,50 @@ public class Uni6Exe07 {
 
     private int[] popularVetor(Scanner s, int vetorInteiro[]) {
         for (int i = 0; i < vetorInteiro.length; i++) {
-            System.out.print("Informe o valor de posição nº " + (i+1) + " :");
-            vetorInteiro[i] = s.nextInt();
-            for (int j  = 0; j < i; j++) {
-                if (vetorInteiro[j] == vetorInteiro[i]) {
-                    System.out.print("ERRO: Já há um valor igual a esse Informe o valor de posição nº " + (i+1) + " :");
-                    vetorInteiro[i] = s.nextInt();
+
+            boolean repetido; // Variável de controle
+            
+            // Usamos um laço 'do-while' para forçar o usuário a digitar até que a regra seja cumprida
+            do {
+                repetido = false; // Começamos assumindo que não é repetido
+                System.out.print("Informe o valor da posição nº " + (i+1) + ": ");
+                vetorInteiro[i] = s.nextInt();
+
+                // Confere com todos os números ANTERIORES
+                for (int j = 0; j < i; j++) {
+                    if (vetorInteiro[j] == vetorInteiro[i]) {
+                        System.out.println("ERRO: Já há um valor igual a esse no vetor!");
+                        repetido = true; // Avisamos que deu erro!
+                        break; // Para o laço 'j', pois já achamos o erro. Vai pedir para digitar de novo.
+                    }
                 }
-            }
+            } while (repetido); // Se for repetido, volta lá pro 'do' e pede de novo!
         }
+        
         return vetorInteiro;
     }
-    private int[] ordenarVetor(int vetorInteiro[], int vetorOrdenado[]) {
 
-        // PENDENTE FAZER O CÓDIGO.
+    private int[] ordenarVetor(int vetorInteiro[]) {
 
+        int[] vetorOrdenado = vetorInteiro.clone();
 
-        return vetorInteiro;
+        for (int i = 0; i < vetorInteiro.length; i++) {
+            
+            for (int j = 0; j < vetorInteiro.length - 1; j++) {
+                
+                // Se o número atual for MAIOR que o número do lado direito...
+                if (vetorOrdenado[j] > vetorOrdenado[j + 1]) {
+                    
+                    // ...hora de fazer a troca usando o "copo vazio" (auxiliar)
+                    int auxiliar = vetorOrdenado[j];       // 1. O copo vazio guarda o valor atual
+                    vetorOrdenado[j] = vetorOrdenado[j + 1];       // 2. O atual recebe o valor do vizinho (que era menor)
+                    vetorOrdenado[j + 1] = auxiliar;       // 3. O vizinho recebe o valor que estava guardado no copo
+                }
+            }
+
+        }
+
+        return vetorOrdenado;
     }
 
     private void informarVetorResultante(int vetorInteiro[], int vetorOrdenado[]) {
